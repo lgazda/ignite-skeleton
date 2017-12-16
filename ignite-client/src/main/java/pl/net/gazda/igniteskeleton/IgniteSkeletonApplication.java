7c481a4391f161aea78e8a78e1086fc0d58f6113
lgazda <lukasz@gazda.net.pl>
2017-12-16 15:08:38 +0100

    Initial commit

---
+++ ignite-client/src/main/java/pl/net/gazda/igniteskeleton/IgniteSkeletonApplication.java
@@ -0,0 +1,12 @@
+package pl.net.gazda.igniteskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteSkeletonApplication.class, args);
+	}
+}
